62d077b403f54f2a240ce2e71d9cfec8c8fd6218
bogdan-constantin13 <bogdan.constantin.it@gmail.com>
2022-11-29 22:02:04 +0200

    JavaScript_3 Curs 29.11.2022

---
+++ m01s04/ex01/app.js
@@ -0,0 +1,11 @@
+var number = prompt('Introdu un numar');
+var message = '';
+var messageElement = document.getElementById('message');
+
+if (number % 2 === 0) {
+  message = 'Numarul este par';
+} else {
+  message = 'Numarul este impar';
+}
+console.log(message);
+messageElement.innerText = message;
